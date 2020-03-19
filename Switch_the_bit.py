d7b87d276dd5744fefdb5a93d29f1dba78772849
Hashir Hameed Niazi <56411583+HashirGJ8842@users.noreply.github.com>
2020-03-19 22:41:57 +0530

    Resolving the infinite loop issue.

--- Switch_the_bit.py
+++ Switch_the_bit.py
@@ -4,13 +4,10 @@ x=list(x.split(' '))
 x=list(map(int,x))
 while True:
     temp=int(input('Enter the index'))
-    for i in range(len(x)):
-        if i==temp:
-            if x[i]==0:
-                x[i]=1
-            elif x[i]==1:
-                x[i]=0
-    print(x)
-    if all([j for j in x if j==0]):
+    if x[temp] == 0:
+        x[temp] = 1
+    else:
+        x[temp] = 0
+    if all([j==0 for j in x]):
         break
 print(x)
